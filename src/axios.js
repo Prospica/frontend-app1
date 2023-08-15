09c3d5574edcc03d9706531010cc9ba377d361da
Prospica <48182826+Prospica@users.noreply.github.com>
2023-08-15 20:18:16 +0700

    update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/', // Localhost Endpoint
+    //baseURL: 'http://localhost:8000/api/', // Localhost Endpoint
     //baseURL: 'https://backend-secondhand-production.up.railway.app/api/', // Localhost Endpoint
-    //baseURL: 'https://secondhand3.herokuapp.com/api/', // Heroku Endpoint
+    baseURL: 'https://backend-app1-4601f9513a01.herokuapp.com/api/', // Heroku Endpoint
     validateStatus: () => true
 })
