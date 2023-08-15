44398f937b3edaf835afa4d30c572f8d8f09d700
Prospica <48182826+Prospica@users.noreply.github.com>
2023-08-15 20:19:14 +0700

    update socketio.js

--- src/socketio.js
+++ src/socketio.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000/"); // Localhost Endpoint
+//export const socket = io("http://localhost:8000/"); // Localhost Endpoint
 //export const socket = io("backend-secondhand-production.up.railway.app"); // Localhost Endpoint
-//export const socket = io("https://secondhand3.herokuapp.com/"); // Heroku Endpoint
+  export const socket = io("https://backend-app1-4601f9513a01.herokuapp.com/"); // Heroku Endpoint
